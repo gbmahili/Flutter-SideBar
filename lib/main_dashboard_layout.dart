51ae03cd9d679549316f586ef5a025b3cc4553c1
Baraka Mahili <georgesmahili@gmail.com>
2019-05-19 14:39:39 -0400

    Added animation to the widget

--- lib/main_dashboard_layout.dart
+++ lib/main_dashboard_layout.dart
@@ -2,58 +2,108 @@ import 'package:flutter/material.dart';
 
 final Color backgroundColor = Color(0xFF4A4A58);
 
-class MenuDashboardPage extends StatelessWidget {
+class MenuDashboardPage extends StatefulWidget {
+  @override
+  _MenuDashboardPageState createState() => _MenuDashboardPageState();
+}
+
+class _MenuDashboardPageState extends State<MenuDashboardPage> {
+  bool isCollapsed = true;
+  double screenWidth, screenHeight;
+
   @override
   Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    screenHeight = deviceSize.height;
+    screenWidth = deviceSize.width;
+    final Duration duration = const Duration(milliseconds: 300);
     return Scaffold(
       backgroundColor: backgroundColor,
       body: Stack(
         children: <Widget>[
           menu(context),
-          // dashboard(),
+          dashboard(context),
         ],
+      ),
+    );
+  }
+
+  Widget menu(context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 18.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new MenuItem(text: 'Dashboard'),
+            new MenuItem(text: 'Messages'),
+            new MenuItem(text: 'Utility Bills'),
+            new MenuItem(text: 'Funds Transfer'),
+            new MenuItem(text: 'Branches'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget dashboard(context) {
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 300),
+      top: isCollapsed ? 0 : 0.2 * screenHeight,
+      bottom: isCollapsed ? 0 : 0.2 * screenHeight,
+      left: isCollapsed ? 0 : 0.6 * screenWidth,
+      right: isCollapsed ? 0 : -0.4 * screenWidth,
+      child: Material(
+        elevation: 8,
+        color: backgroundColor,
+        child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 48),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  InkWell(
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        isCollapsed = !isCollapsed;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Dashboad For User',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-Widget menu(context) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 18.0),
-    child: Align(
-      alignment: Alignment.centerLeft,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Dashboard',
-                style: TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Messages',
-                style: TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Utility Bills',
-                style: TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Funds Transfer',
-                style: TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Branches',
-                style: TextStyle(color: Colors.white, fontSize: 20)),
-          ),
-        ],
-      ),
-    ),
-  );
+class MenuItem extends StatelessWidget {
+  final String text;
+  MenuItem({this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(text, style: TextStyle(color: Colors.white, fontSize: 20)),
+    );
+  }
 }
