c5611792caf7f91fcfc20126ee26d7590fec768e
Baraka Mahili <georgesmahili@gmail.com>
2019-05-20 22:47:02 -0400

    Added listview

--- lib/main_dashboard_layout.dart
+++ lib/main_dashboard_layout.dart
@@ -93,7 +93,7 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
       top: 0,
       bottom: 0,
       left: isCollapsed ? 0 : 0.6 * screenWidth,
-      right: isCollapsed ? 0 : -0.4 * screenWidth,
+      right: isCollapsed ? 0 : -0.2 * screenWidth,
       child: ScaleTransition(
         scale: _scaleAnimation,
         child: Material(
@@ -101,55 +101,79 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
           borderRadius: BorderRadius.all(Radius.circular(40)),
           elevation: 8,
           color: backgroundColor,
-          child: Container(
-            padding: EdgeInsets.only(left: 16, right: 16, top: 48),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    InkWell(
-                        child: Icon(Icons.menu, color: Colors.white),
-                        onTap: () {
-                          setState(() {
-                            isCollapsed
-                                ? _controller.forward()
-                                : _controller.reverse();
-                            isCollapsed = !isCollapsed;
-                          });
-                        }),
-                    Text('Dashboad For User',
-                        style: TextStyle(fontSize: 24, color: Colors.white)),
-                    Icon(Icons.settings, color: Colors.white)
-                  ],
-                ),
-                SizedBox(height: 50),
-                Container(
-                  height: 500,
-                  child: PageView(
-                    controller: PageController(viewportFraction: 0.8),
-                    scrollDirection: Axis.horizontal,
-                    pageSnapping: true,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: ClampingScrollPhysics(),
+            child: Container(
+              padding: EdgeInsets.only(left: 16, right: 16, top: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.redAccent,
-                        width: 100,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.blueAccent,
-                        width: 100,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color: Colors.greenAccent,
-                        width: 100,
-                      ),
+                      InkWell(
+                          child: Icon(Icons.menu, color: Colors.white),
+                          onTap: () {
+                            setState(() {
+                              isCollapsed
+                                  ? _controller.forward()
+                                  : _controller.reverse();
+                              isCollapsed = !isCollapsed;
+                            });
+                          }),
+                      Text('Dashboad For User',
+                          style: TextStyle(fontSize: 24, color: Colors.white)),
+                      Icon(Icons.settings, color: Colors.white)
                     ],
                   ),
-                )
-              ],
+                  SizedBox(height: 50),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction: 0.8),
+                      scrollDirection: Axis.horizontal,
+                      pageSnapping: true,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                  ),
+                  //SizedBox(height: 20),
+                  Break(20.0),
+                  Text('Transactions', style: TextStyle(color: Colors.white, fontSize: 20),),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('MackBook'),
+                        subtitle: Text('Apple'),
+                        trailing: Text('899.99'),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(
+                        height: 16.0,
+                      );
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -168,5 +192,14 @@ class MenuItem extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Text(text, style: TextStyle(color: Colors.white, fontSize: 20)),
     );
+  }
+}
+
+class Break extends StatelessWidget {
+  final height;
+  Break(this.height);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: height);
   }
 }
