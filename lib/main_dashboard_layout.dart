3392514095bbbc7d935f0cbdbb0573cce2435d82
Baraka Mahili <georgesmahili@gmail.com>
2019-05-20 22:10:31 -0400

    Add scale that scales everything in the MaterialApp

--- lib/main_dashboard_layout.dart
+++ lib/main_dashboard_layout.dart
@@ -7,10 +7,26 @@ class MenuDashboardPage extends StatefulWidget {
   _MenuDashboardPageState createState() => _MenuDashboardPageState();
 }
 
-class _MenuDashboardPageState extends State<MenuDashboardPage> {
+class _MenuDashboardPageState extends State<MenuDashboardPage>
+    with SingleTickerProviderStateMixin {
   bool isCollapsed = true;
   double screenWidth, screenHeight;
   final Duration duration = const Duration(milliseconds: 300);
+  AnimationController _controller;
+  Animation<double> _scaleAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.6).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
@@ -60,61 +76,67 @@ class _MenuDashboardPageState extends State<MenuDashboardPage> {
   Widget dashboard(context) {
     return AnimatedPositioned(
       duration: duration,
-      top: isCollapsed ? 0 : 0.2 * screenHeight,
-      bottom: isCollapsed ? 0 : 0.2 * screenHeight,
+      // top: isCollapsed ? 0 : 0.2 * screenHeight,
+      // bottom: isCollapsed ? 0 : 0.2 * screenHeight,
+      top: 0,
+      bottom: 0,
       left: isCollapsed ? 0 : 0.6 * screenWidth,
       right: isCollapsed ? 0 : -0.4 * screenWidth,
-      child: Material(
-        animationDuration: duration,
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        elevation: 8,
-        color: backgroundColor,
-        child: Container(
-          padding: EdgeInsets.only(left: 16, right: 16, top: 48),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  InkWell(
-                      child: Icon(Icons.menu, color: Colors.white),
-                      onTap: () {
-                        setState(() {
-                          isCollapsed = !isCollapsed;
-                        });
-                      }),
-                  Text('Dashboad For User',
-                      style: TextStyle(fontSize: 24, color: Colors.white)),
-                  Icon(Icons.settings, color: Colors.white)
-                ],
-              ),
-              SizedBox(height: 50),
-              Container(
-                height: 200,
-                child: PageView(
-                  controller: PageController(viewportFraction: 0.8),
-                  scrollDirection: Axis.horizontal,
-                  pageSnapping: true,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Material(
+          animationDuration: duration,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          elevation: 8,
+          color: backgroundColor,
+          child: Container(
+            padding: EdgeInsets.only(left: 16, right: 16, top: 48),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.redAccent,
-                      width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.blueAccent,
-                      width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.greenAccent,
-                      width: 100,
-                    ),
+                    InkWell(
+                        child: Icon(Icons.menu, color: Colors.white),
+                        onTap: () {
+                          setState(() {
+                            isCollapsed ? _controller.forward(): _controller.reverse();
+                            isCollapsed = !isCollapsed;
+                          });
+                        }),
+                    Text('Dashboad For User',
+                        style: TextStyle(fontSize: 24, color: Colors.white)),
+                    Icon(Icons.settings, color: Colors.white)
                   ],
                 ),
-              )
-            ],
+                SizedBox(height: 50),
+                Container(
+                  height: 500,
+                  child: PageView(
+                    controller: PageController(viewportFraction: 0.8),
+                    scrollDirection: Axis.horizontal,
+                    pageSnapping: true,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.redAccent,
+                        width: 100,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.blueAccent,
+                        width: 100,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        color: Colors.greenAccent,
+                        width: 100,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
